46545f1523646341e5f2fe54f11a70483fbf2203
GamingGuy003 <bjornfelixh@gmail.com>
2024-03-08 17:54:44 +0000

    Added module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use cali::parser::Parser;
+use log::error;
+
+mod backend;
 
 fn main() {
+    pretty_env_logger::formatted_timed_builder().filter_level(log::LevelFilter::Info).parse_env("LOG").init();
     #[rustfmt::skip]
     let mut parser = Parser::new()
         .add_arg("a", "add", "Adds / replaces binding with optional index", true, true)
@@ -10,7 +14,10 @@ fn main() {
         .add_arg("l", "list", "Lists all bindings", false, false)
         .add_arg("h", "help", "Prints this help dialogue", false, false);
 
-    parser.parse().expect("Failed to parse arguments");
+    parser.parse().err().map(|err| {
+        error!("{}", err);
+        std::process::exit(-1)
+    });
 
     if let Some(_) = parser.get_parsed_argument_long("help") {
         parser
