0e82fa5294ecfecda774e72eab9e0c308ded61a9
GamingGuy003 <bjornfelixh@gmail.com>
2024-03-08 16:51:33 +0100

    Added arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use cali::parser::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    #[rustfmt::skip]
+    let mut parser = Parser::new()
+        .add_arg("a", "add", "Adds / replaces binding with optional index", true, true)
+        .add_arg("r", "remove", "Removes binding with index", true, false)
+        .add_arg("e", "enable", "Enables binding with index", true, false)
+        .add_arg("d", "disable", "Disables binding with index", true, false)
+        .add_arg("l", "list", "Lists all bindings", false, false)
+        .add_arg("h", "help", "Prints this help dialogue", false, false);
+
+    parser.parse().expect("Failed to parse arguments");
+
+    if let Some(_) = parser.get_parsed_argument_long("help") {
+        parser
+            .get_arguments()
+            .iter()
+            .for_each(|arg| println!("\t{}", arg.to_string()));
+        return;
+    }
 }
